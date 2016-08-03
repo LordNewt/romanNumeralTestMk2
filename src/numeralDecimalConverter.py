fdac1335425119cb87e568a59384ba6aae537404
Sam Schmit <schmits@us.ibm.com>
2016-08-03 15:38:22 -0500

    Added converter class

---
+++ src/numeralDecimalConverter.py
@@ -0,0 +1,4 @@
+class NumeralDecimalConverter:
+
+    def __init__(self):
+        pass
---
+++ test/numeralDecimalConverterTest.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.numeralDecimalConverter import NumeralDecimalConverter
+
+
+class NumeralDecimalConverterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.converter = NumeralDecimalConverter()
+
+    #
+    # Base existence tests
+    #
+
+    def test_converter_class_exists(self):
+        self.assertIsNotNone(self.converter)
